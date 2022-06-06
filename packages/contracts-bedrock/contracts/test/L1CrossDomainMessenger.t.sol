814fbf3efec4c1594ad0e60d88c5229b477e9c09
Kelvin Fichter <kelvin@optimism.io>
2022-06-06 15:19:42 -0400

    feat(ctb): add SentMessageV2 event to XDM
    Adds a SentMessageV2 event to the XDM which includes all relevant
    information in the sent message and properly indexes the sender address.

--- packages/contracts-bedrock/contracts/test/L1CrossDomainMessenger.t.sol
+++ packages/contracts-bedrock/contracts/test/L1CrossDomainMessenger.t.sol
@@ -103,6 +103,13 @@ contract L1CrossDomainMessenger_Test is Messenger_Initializer {
            100
         );
 
+        // SentMessageExtraData event
+        vm.expectEmit(true, true, true, true);
+        emit SentMessageExtraData(
+           alice,
+           0
+        );
+
         vm.prank(alice);
         L1Messenger.sendMessage(recipient, hex"ff", uint32(100));
     }
